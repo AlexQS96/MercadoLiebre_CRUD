7876a6c7a0d3d18b82c5f1fde374d111a6296e7d
alexqs96 <alex.rps96@gmail.com>
2021-09-12 15:57:30 -0300

    TP

---
+++ public/js/button.js
@@ -0,0 +1,8 @@
+function burgerButton() {
+    var x = document.getElementById("BButton");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
